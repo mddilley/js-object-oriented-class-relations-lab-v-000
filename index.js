181577afa91ef23f9c86a1b81b08418f834bb3f1
Michael Dilley <mike.dilley@gmail.com>
2019-02-28 10:19:39 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,76 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let driverId = 0;
+
+class Driver {
+  constructor(name){
+    this.name = name;
+    this.id = ++driverId;
+    store.drivers.push(this);
+  }
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.driverId === this.id;
+      }.bind(this)
+    );
+  }
+  passengers() {
+    const trips = this.trips();
+    return trips.map(
+      function(trip) {
+        return trip.passenger()
+      }.bind(this)
+    );
+  }
+}
+
+let passengerId = 0;
+
+class Passenger {
+  constructor(name){
+    this.name = name;
+    this.id = ++passengerId;
+    store.passengers.push(this);
+  }
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.passengerId === this.id;
+      }.bind(this)
+    );
+  }
+  drivers() {
+    const trips = this.trips();
+    return trips.map(
+      function(trip) {
+        return trip.driver()
+      }.bind(this)
+    );
+  }
+}
+
+let tripId = 0;
+
+class Trip {
+  constructor(driver, passenger){
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = ++tripId;
+    store.trips.push(this);
+  }
+  passenger() {
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId;
+      }.bind(this)
+    );
+  }
+  driver() {
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId;
+      }.bind(this)
+    );
+  }
+}
